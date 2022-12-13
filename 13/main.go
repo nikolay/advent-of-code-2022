a222b289e636840170a7a973a34c9201ced20763
Nikolay Kolev <nikolay@users.noreply.github.com>
2022-12-12 23:53:37 -0800

    feat: solutions for day 13

---
+++ 13/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Item struct {
+	hasValue  bool
+	value     int
+	list      []Item
+	isDivider bool
+}
+
+func Parse(line string, startPos int) (Item, int, error) {
+	pos := startPos
+	if line[pos] == '[' {
+		pos++
+		result := Item{}
+		result.list = []Item{}
+		for line[pos] != ']' {
+			item, newPos, err := Parse(line, pos)
+			if err != nil {
+				return Item{}, -1, err
+			}
+			result.list = append(result.list, item)
+			pos = newPos
+			if ch := line[pos]; ch == ',' {
+				pos++
+			} else if ch != ']' {
+				return Item{}, -1, fmt.Errorf("invalid command: %v", line)
+			}
+		}
+		return result, pos + 1, nil
+	}
+	result := Item{hasValue: true}
+	for line[pos] >= '0' && line[pos] <= '9' {
+		pos++
+	}
+	var err error
+	if result.value, err = strconv.Atoi(line[startPos:pos]); err != nil {
+		return Item{}, -1, err
+	}
+	return result, pos, nil
+}
+
+func Sign(v int) int {
+	if v < 0 {
+		return -1
+	}
+	if v > 0 {
+		return +1
+	}
+	return 0
+}
+
+func Compare(left, right Item) int {
+	if left.hasValue {
+		if right.hasValue {
+			return Sign(left.value - right.value)
+		}
+		return Compare(Item{list: []Item{left}}, right)
+	}
+	if right.hasValue {
+		return Compare(left, Item{list: []Item{right}})
+	}
+	l, r := len(left.list), len(right.list)
+	for i := 0; i < l && i < r; i++ {
+		if v := Compare(left.list[i], right.list[i]); v != 0 {
+			return v
+		}
+	}
+	return Sign(l - r)
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	items := []Item{}
+	for row := 0; scanner.Scan(); row++ {
+		line := strings.TrimSpace(scanner.Text())
+		if len(line) == 0 {
+			continue
+		}
+		if item, _, err := Parse(line, 0); err != nil {
+			log.Fatal(err)
+		} else {
+			items = append(items, item)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Part 1
+	sum := 0
+	for i := 0; i < len(items); i += 2 {
+		if Compare(items[i], items[i+1]) < 0 {
+			sum += i/2 + 1
+		}
+	}
+	fmt.Println(sum)
+
+	// Part 2
+	for _, v := range []int{2, 6} {
+		items = append(items, Item{isDivider: true, list: []Item{Item{list: []Item{Item{hasValue: true, value: v}}}}})
+	}
+	for i := 0; i < len(items)-1; i++ {
+		for j := i + 1; j < len(items); j++ {
+			if Compare(items[i], items[j]) > 0 {
+				temp := items[i]
+				items[i] = items[j]
+				items[j] = temp
+			}
+		}
+	}
+	product := 1
+	for index, item := range items {
+		if item.isDivider {
+			product *= index + 1
+		}
+	}
+	fmt.Println(product)
+}
