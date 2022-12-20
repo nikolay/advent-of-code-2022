abd7c2b29002f1b31a0f3e2bb7186a9c0c4b09b7
Nikolay Kolev <nikolay@users.noreply.github.com>
2022-12-20 00:41:17 -0800

    feat: solutions for day 20

---
+++ 20/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Modulus(a, n int) int {
+	remainder := a % n
+	if remainder < 0 && n > 0 || remainder > 0 && n < 0 {
+		return remainder + n
+	}
+	return remainder
+}
+
+func Mix(numbers []int, decryptionKey int, iterations int) int {
+	count := len(numbers)
+	decryptedNumbers := make([]int, count)
+	indices := make([]int, count)
+	for i, number := range numbers {
+		decryptedNumbers[i] = number * decryptionKey
+		indices[i] = i
+	}
+	for n := 0; n < iterations; n++ {
+		for i := 0; i < count; i++ {
+			oldPos := -1
+			for j := 0; j < count; j++ {
+				if indices[j] == i {
+					oldPos = j
+					break
+				}
+			}
+			newPos := Modulus(oldPos + decryptedNumbers[i], count-1)
+
+			slice := append(indices[:oldPos], indices[oldPos+1:]...)
+			mixed := make([]int, newPos+1, count)
+			copy(mixed, slice[:newPos])
+			mixed[newPos] = i
+			mixed = append(mixed, slice[newPos:]...)
+
+			indices = mixed
+		}
+	}
+	for i := 0; i < count; i++ {
+		if decryptedNumbers[i] == 0 {
+			for j := 0; j < count; j++ {
+				if indices[j] == i {
+					sum := 0
+					for _, k := range []int{1000, 2000, 3000} {
+						sum += decryptedNumbers[indices[Modulus(j+k, count)]]
+					}
+					return sum
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func Solve1(numbers []int) int {
+	return Mix(numbers, 1, 1)
+}
+
+func Solve2(numbers []int) int {
+	return Mix(numbers, 811589153, 10)
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var numbers []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if len(line) == 0 {
+			continue
+		}
+		number, _ := strconv.Atoi(line)
+		numbers = append(numbers, number)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(Solve1(numbers))
+	fmt.Println(Solve2(numbers))
+}
